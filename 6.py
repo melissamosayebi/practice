bd4d99c68acee8ad12ada786bb28643b3cb4b6ad
melissa <melissamsayebi78@gmail.com>
2024-01-16 00:52:19 +0330

    finished

--- 6.py
+++ 6.py
@@ -1,22 +1,9 @@
-try:
-    n=int(input("how many? "))
-    a=[]
-    i=0
-    while i<n:
-        b=input("enter a num: ")
+n=float(input("how many? "))
+n=int(n)
+a=[]
+i=0
+while i<n:
+        b=int(input("enter a num: "))
         a.append(b)
         i+=1
-    print(a[ : :-1])
-except ValueError:
-    print("rong num")
-    n=input("how many? ")
-    if n!=int(n):
-        while n!=int(n):
-            n=int(input("how many? "))
-    a=[]
-    i=0
-    while i<n:
-        b=input("enter a num: ")
-        a.append(b)
-        i+=1
-    print(a[ : :-1])
+print(a[ : :-1])
