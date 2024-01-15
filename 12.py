ee3dda5c559d32df6628fbbf4e8d4e3cd4aa32a7
melissa <melissamsayebi78@gmail.com>
2024-01-15 23:46:38 +0330

    selection with def

--- 12.py
+++ 12.py
@@ -1,14 +1,15 @@
-a=[4,5,8,3,6,1,9,7]
-for i in range(len(a)-1):
-    j=i
-    x=j
-    while j<len(a)-1:
-        if a[j+1]<a[x]:
-            x=j+1
-        j+=1
-    s=x
-    a.insert(i,a[x])
-    del a[s+1]
-    #print(a)
-    i+=1
-print(a)
+def selection(a):
+    for i in range(len(a)-1):
+        j=i
+        x=j
+        while j<len(a)-1:
+            if a[j+1]<a[x]:
+                x=j+1
+            j+=1
+        s=x
+        a.insert(i,a[x])
+        del a[s+1]
+        #print(a)
+        i+=1
+    return a
+print(selection([4,5,8,3,6,1,9,7]))
