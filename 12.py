78a24735acc36940e9021a5f787a97f788b1a83e
melissa <melissamsayebi78@gmail.com>
2024-01-15 23:42:08 +0330

    selection without def

---
+++ 12.py
@@ -0,0 +1,14 @@
+a=[4,5,8,3,6,1,9,7]
+for i in range(len(a)-1):
+    j=i
+    x=j
+    while j<len(a)-1:
+        if a[j+1]<a[x]:
+            x=j+1
+        j+=1
+    s=x
+    a.insert(i,a[x])
+    del a[s+1]
+    #print(a)
+    i+=1
+print(a)
