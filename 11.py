dd6ba7a8671725c4f2210ca96e8cdb28a3cd27cd
melissa <melissamsayebi78@gmail.com>
2024-01-15 22:47:37 +0330

    fibu with def

--- 11.py
+++ 11.py
@@ -1,9 +1,10 @@
-a=[0,1]
-n=int(input("n: "))
-x=0
-i=0
-for i in range(n-2):
-    x=a[i]+a[i+1]
-    a.append(x)
-    i+=1
-print(a)
+def fibu(n):    
+    a=[0,1]
+    x=0
+    i=0
+    for i in range(n-2):
+        x=a[i]+a[i+1]
+        a.append(x)
+        i+=1
+    return a
+print(fibu(10))
